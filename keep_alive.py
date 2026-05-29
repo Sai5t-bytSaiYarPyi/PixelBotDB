a3ad39d322a992b4674453ea541c750003be9af0
NaJu <izo52928@gmail.com>
2026-05-29 16:52:23 +0630

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running beautifully!"
+    return "✅ Pixel Verification Bot is running!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True
     t.start()
